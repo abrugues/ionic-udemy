935b254e720e5062c42321eae97b2d3649490769
abrugues <albert.brugues@gmail.com>
2017-10-06 16:46:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ionic 2 / Ionic 3 - Build iOS & Android Apps with Angular
+
+This repository contains the three apps developed through the [Ionic course on Udemy](https://www.udemy.com/ionic-2-the-practical-guide-to-building-ios-android-apps/) taught by Maximilian Schwarzmüller.
+
+## Contents
+Each app focuses on different features, which are listed below.
+- Favorite Quotes: Navigation, Pages and Components
+- The Recipe Book: User Input, Forms, Data Management, Auth and Http
+- Awesome Places: Native Device Features and Storage
---
+++ awesome-places-app/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from "@ionic-native/geolocation";
+import { Camera } from "@ionic-native/camera";
+import { File } from "@ionic-native/file";
+import { IonicStorageModule } from "@ionic/storage";
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddPlacePage } from "../pages/add-place/add-place";
+import { PlacePage } from "../pages/place/place";
+import { SetLocationPage } from "../pages/set-location/set-location";
+import { AgmCoreModule } from "@agm/core";
+import { PlacesService } from "../services/places";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AddPlacePage,
+    PlacePage,
+    SetLocationPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAINl5Mq_MZbtY-0qIjWZhKOZTAYUDJx34'
+    }),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddPlacePage,
+    PlacePage,
+    SetLocationPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation,
+    Camera,
+    PlacesService,
+    File
+  ]
+})
+export class AppModule {}
